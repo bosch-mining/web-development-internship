37e0151a9995198df35eb56367e0e1c501c50c8a
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-11 13:16:03 -0300

    fix: create and delete throwing the correct http status

--- backend/src/main/java/com/victorpereira/internship/resources/TaskResource.java
+++ backend/src/main/java/com/victorpereira/internship/resources/TaskResource.java
@@ -3,6 +3,7 @@ package com.victorpereira.internship.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorpereira.internship.models.Task;
@@ -35,6 +37,7 @@ public class TaskResource {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public void createTask(@RequestBody Task task) {
 		service.createTask(task);
 	}
@@ -45,6 +48,7 @@ public class TaskResource {
 	}
 	
 	@DeleteMapping(value="/{taskId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteTask(@PathVariable Integer taskId) {
 		service.deleteTask(taskId);
 	}
