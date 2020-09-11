22ed73e9a3337513a94c2978a0878d86855cd12d
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-11 13:45:46 -0300

    docs: swagger documentation

---
+++ backend/src/main/java/com/victorpereira/internship/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.victorpereira.internship.config;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket arsenalApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.victorpereira.internship")).paths(PathSelectors.any())
+				.build().apiInfo(metaInfo());
+	}
+
+	private ApiInfo metaInfo() {
+		ApiInfo apiInfo = new ApiInfo("Tasks REST API", "REST API to control tasks",
+				"1.0", "Terms of Service",
+				new Contact("Victor Pereira Cordeiro", "https://github.com/victorpereira01", "victor.cordeiro2@gmail.com"), null,
+				null, new ArrayList<VendorExtension>());
+
+		return apiInfo;
+
+	}
+}
--- backend/src/main/java/com/victorpereira/internship/resources/TaskResource.java
+++ backend/src/main/java/com/victorpereira/internship/resources/TaskResource.java
@@ -18,35 +18,44 @@ import org.springframework.web.bind.annotation.RestController;
 import com.victorpereira.internship.models.Task;
 import com.victorpereira.internship.services.TaskService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/tasks")
+@Api(value="Tasks REST API")
 @CrossOrigin(origins = "*")
 public class TaskResource {
 	
 	@Autowired
 	private TaskService service;
 	
+	@ApiOperation(value="Return a list with all tasks")
 	@GetMapping
 	public List<Task> listTasks() {
 		return service.listTasks();
 	}
 	
+	@ApiOperation(value="Returns a unique task")
 	@GetMapping(value="/{taskId}")
 	public Task getTask(@PathVariable Integer taskId) {
 		return service.getTask(taskId);
 	}
 	
+	@ApiOperation(value="Saves a task")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createTask(@RequestBody Task task) {
 		service.createTask(task);
 	}
 	
+	@ApiOperation(value="Updates a task")
 	@PutMapping(value="/{taskId}")
 	public void editTask(@PathVariable Integer taskId, @RequestBody Task task) {
 		service.editTask(taskId, task);
 	}
 	
+	@ApiOperation(value="Deletes a task")
 	@DeleteMapping(value="/{taskId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteTask(@PathVariable Integer taskId) {
